a03c41d2be9746b8ad6acf172c7ec7c4d8a4f908
KarinaMikaelson <mikael.kar7@gmail.com>
2024-03-20 22:40:39 -0700

    first commit

---
+++ src/main/java/org/example/e10/DuplicateElement.java
@@ -0,0 +1,17 @@
+package org.example.e10;
+
+public class DuplicateElement {
+    public static void main(String[] args) {
+
+        String [] elements = {"car", "bike", "nose", "home", "nose", "work", "pen", "bike"};
+
+        for(int i = 0; i < elements.length - 1; i++){
+            for(int j = i + 1; j < elements.length; j++){
+                //// Check if two string elements are equal and not the same element.
+                if((elements[i].equals(elements[j])) && (i != j)){
+                    System.out.println("Duplicate element is: "+elements[j]);
+                }
+            }
+        }
+    }
+}
